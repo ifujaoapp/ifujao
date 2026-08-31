635ea4615b7b1ecdc5ea260bb9c6d4281bb31d3b
m-rollo <marciorollo@gmail.com>
2026-08-31 09:43:08 -0300

    Quebra do texto do balão: 'perdido/encontrado' em linha abaixo

--- components/home/MapArea.tsx
+++ components/home/MapArea.tsx
@@ -295,7 +295,7 @@ export function MapArea(props: MapAreaProps) {
             style={[styles.speechBubble, { opacity: bubbleOpacity }]}
           >
             <Text style={styles.speechBubbleText}>
-              Toque para{"\n"}reportar um pet perdido/encontrado
+              Toque para reportar um pet{"\n"}perdido/encontrado
             </Text>
             <View style={styles.speechBubbleArrow} />
           </Animated.View>
